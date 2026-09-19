fd65e776ef3a7bf8611bde85adee75139454f1c1
zyrk0794 <zyrk0794@gmail.com>
2026-09-19 11:30:39 +0200

    Update Init.lua

--- src/Init.lua
+++ src/Init.lua
@@ -16,7 +16,7 @@ local ReGui = {
 	Version = "1.4.7",
 	Author = "Depso",
 	License = "MIT",
-	Repository = "https://github.com/Jibbefr/Dear-ReGui-Edited/",
+	Repository = "https://github.com/zyrk0794/ReGui/",
 
 	--// Configuration
 	Debug = false,
